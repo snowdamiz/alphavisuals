c21a7dc6752a0b21586d3e972aa9794791e41e0d
120356aa <yurlovandrew@gmail.com>
2019-10-11 16:14:38 -0700

    header file structure

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HeaderWrap } from './headerStyles';
+
+function Header() {
+    return (
+        <HeaderWrap>
+            
+        </HeaderWrap>
+    )
+}
+
+export default Header;
---
+++ src/components/header/headerStyles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const HeaderWrap = styled.div`
+    width: 100%;
+    border: 1px solid red;
+`;
