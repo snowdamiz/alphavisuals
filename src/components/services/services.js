f77e7565cf3c0a7075cf5a7e6977af3e599f5443
120356aa <yurlovandrew@gmail.com>
2019-11-03 17:23:57 -0800

    services tablet and desktop

--- src/components/services/services.js
+++ src/components/services/services.js
@@ -4,82 +4,89 @@ import { ServicesWrap } from './servicesStyles';
 function Services () {
     return (
         <ServicesWrap>
-            <div className="service">
-                <div className="service_content">
-                    <h1>Car Photos</h1>
-                    <div className="line"></div>
-                    <p className="price" ><span>Price: </span> $50</p>
-                    <p className="location"><span>Locations: </span> One location</p>
-                    <p className="description"><span>Description: </span>Up to 15 edited photos of your vehicle.</p>
+            <div className="service_sec">
+                <div className="service">
+                    <div className="service_content">
+                        <h1>Car Photos</h1>
+                        <div className="line"></div>
+                        <p className="price" ><span>Price: </span> $50</p>
+                        <p className="location"><span>Locations: </span> One location</p>
+                        <p className="description"><span>Description: </span>Up to 15 edited photos of your vehicle.</p>
+                    </div>
+                    <div className="service_cta">
+                        <button>Set Appointment</button>
+                    </div>
                 </div>
-                <div className="service_cta">
-                    <button>Set Appointment</button>
+
+                <div className="service">
+                    <div className="service_content">
+                        <h1>Rolling Shots</h1>
+                        <div className="line"></div>
+                        <p className="price" ><span>Price: </span> $80</p>
+                        <p className="location"><span>Locations: </span> One location</p>
+                        <p className="description"><span>Description: </span>Three to six photos of up to 2 vehicle.</p>                                    
+                    </div>
+                    <div className="service_cta">
+                        <button>Set Appointment</button>
+                    </div>
                 </div>
             </div>
 
-            <div className="service">
-                <div className="service_content">
-                    <h1>Rolling Shots</h1>
-                    <div className="line"></div>
-                    <p className="price" ><span>Price: </span> $80</p>
-                    <p className="location"><span>Locations: </span> One location</p>
-                    <p className="description"><span>Description: </span>Three to six photos of up to 2 vehicle.</p>                                    
+
+            <div className="service_sec">
+                <div className="service">
+                    <div className="service_content">
+                        <h1>Couples Shoot</h1>
+                        <div className="line"></div>
+                        <p className="price" ><span>Price: </span> $100</p>
+                        <p className="location"><span>Locations: </span> Up to two location</p>
+                        <p className="description"><span>Description: </span>Up to 25 couples photos.</p>                                    
+                    </div>
+                    <div className="service_cta">
+                        <button>Set Appointment</button>
+                    </div>
                 </div>
-                <div className="service_cta">
-                    <button>Set Appointment</button>
+
+                <div className="service">
+                    <div className="service_content">
+                        <h1>Engagement Shoot</h1>
+                        <div className="line"></div>
+                        <p className="price" ><span>Price: </span> $150</p>
+                        <p className="location"><span>Locations: </span> Up to two location</p>
+                        <p className="description"><span>Description: </span>Up to 30 engagement photos.</p>                 
+                    </div>
+                    <div className="service_cta">
+                        <button>Set Appointment</button>
+                    </div>
                 </div>
             </div>
 
-            <div className="service">
-                <div className="service_content">
-                    <h1>Couples Shoot</h1>
-                    <div className="line"></div>
-                    <p className="price" ><span>Price: </span> $100</p>
-                    <p className="location"><span>Locations: </span> Up to two location</p>
-                    <p className="description"><span>Description: </span>Up to 25 couples photos.</p>                                    
+            <div className="service_sec">
+                <div className="service">
+                    <div className="service_content">
+                        <h1>RDM Mini Shoot</h1>
+                        <div className="line"></div>
+                        <p className="price" ><span>Price: </span> $30</p>
+                        <p className="location"><span>Locations: </span> Single nearby location</p>
+                        <p className="description"><span>Description: </span>Up to 10 photos of your vehicle.</p>
+                    </div>
+                    <div className="service_cta">
+                        <button>Set Appointment</button>
+                    </div>
                 </div>
-                <div className="service_cta">
-                    <button>Set Appointment</button>
-                </div>
-            </div>
 
-            <div className="service">
-                <div className="service_content">
-                    <h1>Engagement Shoot</h1>
-                    <div className="line"></div>
-                    <p className="price" ><span>Price: </span> $150</p>
-                    <p className="location"><span>Locations: </span> Up to two location</p>
-                    <p className="description"><span>Description: </span>Up to 30 engagement photos.</p>                 
-                </div>
-                <div className="service_cta">
-                    <button>Set Appointment</button>
-                </div>
-            </div>
-
-            <div className="service">
-                <div className="service_content">
-                    <h1>RDM Mini Shoot</h1>
-                    <div className="line"></div>
-                    <p className="price" ><span>Price: </span> $30</p>
-                    <p className="location"><span>Locations: </span> Single nearby location</p>
-                    <p className="description"><span>Description: </span>Up to 10 photos of your vehicle.</p>
-                </div>
-                <div className="service_cta">
-                    <button>Set Appointment</button>
-                </div>
-            </div>
-
-            
-            <div className="service">
-                <div className="service_content">
-                    <h1>Car Profile</h1>
-                    <div className="line"></div>
-                    <p className="price" ><span>Price: </span> $100</p>
-                    <p className="location"><span>Locations: </span> Professionally chosen  location</p>
-                    <p className="description"><span>Description: </span>Up to 15 photos of your vehicle.</p>                                        
-                </div>
-                <div className="service_cta">
-                    <button>Set Appointment</button>
+                
+                <div className="service">
+                    <div className="service_content">
+                        <h1>Car Profile</h1>
+                        <div className="line"></div>
+                        <p className="price" ><span>Price: </span> $100</p>
+                        <p className="location"><span>Locations: </span> Professionally chosen  location</p>
+                        <p className="description"><span>Description: </span>Up to 15 photos of your vehicle.</p>                                        
+                    </div>
+                    <div className="service_cta">
+                        <button>Set Appointment</button>
+                    </div>
                 </div>
             </div>
         </ServicesWrap>
--- src/components/services/servicesStyles.js
+++ src/components/services/servicesStyles.js
@@ -9,85 +9,135 @@ export const ServicesWrap = styled.div`
     flex-flow: column nowrap;
     align-items: center;
 
-    .service {
-        width: 90%;
-        // box-shadow: 2px 2px 10px #717171;
-        padding: 20px;
-        margin-top: 80px;
-        margin-bottom: 20px;
+    .service_sec {
+        width: 100%;
+        display: flex;
+        flex-flow: column nowrap;
+        align-items: center;
 
-        .service_content {
-            width: 100%;
-            // border: 1px solid red;
-
-            h1 {
-                font-size: 44px;
-                font-family: ${fonts.test3};
-                color: #414141;
-            }
-
-            .line {
-                width: 60px;
-                height: 4px;
-                border-radius: 2px;
-                background-color: #00CFB2;
-                margin-top: 10px;
-            }
-
-            .description {
-                font-size: 17px;
-                font-family: ${fonts.primary};
-
-                span {
-                    font-weight: 500;
-                    font-style: italic;
-                }
-            }
-
-            .location {
-                font-size: 17px;
-                font-family: ${fonts.primary};
-                // margin-top: 20px;
-
-                span {
-                    font-weight: 500;
-                    font-style: italic;
-                }
-            }
-
-            .price {
-                font-size: 17px;
-                font-family: ${fonts.primary};
-                margin-top: 30px;
-
-                span {
-                    font-weight: 500;
-                    font-style: italic;
-                }
-            }
+        ${media.largeMb} {
+            width: 480px;
         }
 
-        .service_cta {
-            width: 100%;
-            // border: 1px solid green;
+        ${media.largeTb} {
+            width: 90%;
+            // border: 1px solid red;
+            flex-flow: row nowrap;
+            justify-content: space-between;
+        }
 
-            button:nth-of-type(1) {
-                border: 2px solid #414141;
-                background-color: transparent;
-                padding: 8px 12px;
-                margin-top: 20px;
-                font-size: 16px;
-                font-family: ${fonts.primary};
-                font-weight: 500;
+        ${media.desktop} {
+            width: 1000px;
+        }
+        
+        .service {
+            width: 90%;
+            // box-shadow: 2px 2px 10px #717171;
+            padding: 20px;
+            margin-top: 80px;
+            margin-bottom: 20px;
+            // border: 1px solid red;
+            
+            ${media.largeMb} {
+                display: flex;
+                flex-flow: column nowrap;
+                justify-content: center;
+                align-items: center;
+                margin-top: 100px;
+                margin-bottom: 40px;
             }
 
-            button:nth-of-type(2) {
-                border: none;
-                background-color: transparent;
-                padding: 8px 12px;
-                font-size: 15px;
-                font-family: ${fonts.primary}l
-                font-weight: 500;
+            ${media.largeTb} {
+                width: 440px;
+                margin-top: 120px;
+                margin-bottom: 50px;
+            }
+
+            ${media.desktop} {
+                margin-top: 140px;
+                margin-bottom: 60px;
+            }
+    
+            .service_content {
+                width: 100%;
+                // border: 1px solid red;
+                // ${media.largeMb} {
+                //     display: flex;
+                //     flex-flow: column nowrap;
+                //     justify-content: center;
+                //     // align-items: center;
+                // }
+    
+                h1 {
+                    font-size: 44px;
+                    font-family: ${fonts.test3};
+                    color: #414141;
+                }
+    
+                .line {
+                    width: 60px;
+                    height: 4px;
+                    border-radius: 2px;
+                    background-color: #00CFB2;
+                    margin-top: 10px;
+                }
+    
+                .description {
+                    font-size: 17px;
+                    font-family: ${fonts.primary};
+    
+                    span {
+                        font-weight: 500;
+                        font-style: italic;
+                    }
+                }
+    
+                .location {
+                    font-size: 17px;
+                    font-family: ${fonts.primary};
+                    // margin-top: 20px;
+    
+                    span {
+                        font-weight: 500;
+                        font-style: italic;
+                    }
+                }
+    
+                .price {
+                    font-size: 17px;
+                    font-family: ${fonts.primary};
+                    margin-top: 30px;
+    
+                    span {
+                        font-weight: 500;
+                        font-style: italic;
+                    }
+                }
+            }
+    
+            .service_cta {
+                width: 100%;
+                // border: 1px solid green;
+                // text-align: center;
+    
+                button:nth-of-type(1) {
+                    border: 2px solid #414141;
+                    background-color: transparent;
+                    padding: 8px 12px;
+                    margin-top: 20px;
+                    font-size: 16px;
+                    font-family: ${fonts.primary};
+                    font-weight: 500;
+                }
+    
+                button:nth-of-type(2) {
+                    border: none;
+                    background-color: transparent;
+                    padding: 8px 12px;
+                    font-size: 15px;
+                    font-family: ${fonts.primary}l
+                    font-weight: 500;
+                }
             }
         }
     }
