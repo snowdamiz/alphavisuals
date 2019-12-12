2734e0e478b4c4ef0e3d5c1bad1bdb3971d98c61
120356aa <yurlovandrew@gmail.com>
2019-12-12 00:52:43 -0800

    redirect timeout

--- src/components/purchase/purchase.js
+++ src/components/purchase/purchase.js
@@ -71,7 +71,7 @@ function Purchase () {
                         if (shoot) {
                             if (time) {
                                 Axios.post("https://alphavisuals.herokuapp.com/api/send", product);
-                                // history.push('/');
+                                setTimeout(() => history.push('/'), 3000);
                                 toast('Success! Your Request Has Been Submitted', { type: 'success' });
                             } else setErr("Please Enter Prefered Time")
                         } else setErr("Please Select Type of Shoot")
